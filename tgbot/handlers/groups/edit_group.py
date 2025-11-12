ccd5ed9e38e7ecde682a8e72eef28d90d989a961
Abdulmajid Yunusov <goldendevuz@gmail.com>
2025-11-12 07:47:44 +0500

    2025.11.12/07:47

---
+++ tgbot/handlers/groups/edit_group.py
@@ -0,0 +1,37 @@
+import io
+
+from aiogram import types, Router
+from aiogram.types import Message
+
+from filters import IsGroup
+
+edit_group_router = Router()
+
+
+@edit_group_router.message(IsGroup(), Command("set_photo", prefixes="!/"), AdminFilter())
+async def set_new_photo(message: Message):
+    source_message = message.reply_to_message
+    photo = source_message.photo[-1]
+    photo = await photo.download(destination=io.BytesIO())
+    input_file = types.InputFile(photo)
+    #1-usul
+    await message.chat.set_photo(photo=input_file)
+
+
+@edit_group_router.message(IsGroup(), Command("set_title", prefixes="!/"), AdminFilter())
+async def set_new_title(message: Message):
+    source_message = message.reply_to_message
+    title = source_message.text
+    #2-usul
+    await bot.set_chat_title(message.chat.id, title=title)
+
+
+
+@edit_group_router.message(IsGroup(), Command("set_description", prefixes="!/"), AdminFilter())
+async def set_new_description(message: Message):
+    source_message = message.reply_to_message
+    description = source_message.text
+    # 1-usul
+    # await bot.set_chat_description(message.chat.id, description=description)
+    # 2-usul
+    await message.chat.set_description(description=description)
